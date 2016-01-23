edc5ec499c0d56e18f1b37ecbd631f9876262351
Michel Frechette <mifrec@hotmail.com>
2016-01-23 13:43:37 -0500

    Strings and Variables

--- Introductions.java
+++ Introductions.java
@@ -5,7 +5,9 @@ public class Introductions {
     public static void main(String[] args) {
         Console console = System.console();
         // Welcome to the introductions program!  Your code goes below here
-        console.printf("Hello, my name is Michel");
-
+        String firstName = console.readLine("What is your name?  ");
+        // thisIsAnExampleOfCamelCasing
+        console.printf("Hello, my name is %s\n", firstName);
+        console.printf("%s is learning how to write Java\n", firstName);
   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Treehouse Java Track
 ## Java Basics
 1. Getting Started with Java
+  * Introduction to Your Tools
+  * Strings and Variables
